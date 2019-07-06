d6573d51d1d3f0c2e6fbb4d10523e8ebbb0cc5bd
lov <sb_lov@sina.com>
2019-07-06 14:50:32 +0800

    xfun-0.11

--- djangoDemo/xFun/models.py
+++ djangoDemo/xFun/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+class faceJavManager(models.Manager):
+    def get_queryset(self):
+        return super(faceJavManager, self).get_queryset().filter()
+
 # Create your models here.
+class faceJav(models.Model):
+    fjObject = faceJavManager()
+
+    title = models.CharField(db_column ='title', max_length=50)
+    imgUrl = models.CharField(db_column ='IMGURL', max_length=500)
+    pageUrl = models.CharField(db_column ='PAGEURL', max_length=500)
+    description = models.CharField(db_column ='description',max_length=500 )
+    updateTime = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'title:%s;img:%s;page:%s;descr:%s'%(self.title, self.imgUrl, self.pageUrl, self.description)
+
+    class Meta:
+        db_table = 't_facejav'
+        ordering = ['id']
+
+    @classmethod
+    def createFaceJav(cls, tit, img, page, des):
+        fj = cls(title=tit, imgUrl=img, pageUrl=page, description=des)
+        return fj
--- djangoDemo/xFun/views.py
+++ djangoDemo/xFun/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import faceJav
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'xFun/index.html')
+    fjList  = faceJav.fjObject.get_queryset()
+
+    # print(fjList)
+
+    return render(request, 'xFun/index.html',{'fjList':fjList})
 
 def allView(request, htmlName):
     return render(request, 'xFun/'+htmlName+'.html')
